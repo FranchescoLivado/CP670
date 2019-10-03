dfbd6732092f5c607cf58755525adb14c83fff87
Franchesco <livadofranchesco@gmail.com>
2019-10-03 19:42:04 -0400

    Assignment2Part4 TBC

--- app/src/main/java/com/example/myapplication/ChatWIndow.java
+++ app/src/main/java/com/example/myapplication/ChatWIndow.java
@@ -2,11 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -25,12 +32,48 @@ public class ChatWindow extends AppCompatActivity {
         search = (EditText) findViewById(R.id.textView6);
         list = (ListView) findViewById(R.id.listView);
 
+        ChatAdapter messageAdapter = new ChatAdapter(this); //unsure about the context
+        list.setAdapter(messageAdapter);
+
         send.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 String listItem = search.getText().toString();
                 array.add(listItem);
+                messageAdapter.notifyAdapterSetChanged(); //accessed within inner class, needs to be declared final?
+                search.setText(""); //unsure about this
             }
         });
 
+
+    }
+
+    private class ChatAdapter extends ArrayAdapter<String> {
+        public ChatAdapter(Context ctx){
+            super(ctx, 0);
+        }
+
+        public int getCount(){
+            return array.size();
+        }
+
+        public String getPosition(int position){
+            return array.get(position);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result = null;
+            if (position%2 == 0){
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            }
+            else{
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+            }
+            TextView message = (TextView) result.findViewById(R.id.message_text);
+            message.setText(getItem(position));
+
+            return result;
+        }
     }
 }
