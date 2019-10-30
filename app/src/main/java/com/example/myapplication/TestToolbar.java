8419ad9913facbbe2921f5000bc58e0dd8dfd56d
Franchesco <livadofranchesco@gmail.com>
2019-10-29 23:26:49 -0400

    Finished Assignment2Part4

--- app/src/main/java/com/example/myapplication/TestToolbar.java
+++ app/src/main/java/com/example/myapplication/TestToolbar.java
@@ -1,21 +1,29 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class TestToolbar extends AppCompatActivity {
-
+    EditText new_message;
+    String snackbarMsg = "You selected item 1";
+    Snackbar snackbar;
+    TextView snackBarText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +39,7 @@ public class TestToolbar extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     public boolean onCreateOptionsMenu(Menu m){
@@ -44,13 +53,45 @@ public class TestToolbar extends AppCompatActivity {
 
         switch(id){
             case R.id.action_one:
-                Snackbar.make(getWindow().getDecorView(), "You selected item 1", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(getWindow().getDecorView(), snackbarMsg, Snackbar.LENGTH_LONG).show();
                 Log.d("Toolbar", "Option 1 selected");
                 break;
             case R.id.action_two:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(TestToolbar.this);
+                //Do you want to go back? Prompt
+                builder.setTitle(R.string.goBack);
+                //Buttons
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        //User clicked ok, finish activity
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        //User cancelled so do nothing
+                    }
+                });
+                builder.show();
                 Log.d("Toolbar", "Option 2 selected");
                 break;
             case R.id.action_three:
+                AlertDialog.Builder cBuilder = new AlertDialog.Builder(TestToolbar.this);
+                LayoutInflater inflater = TestToolbar.this.getLayoutInflater();
+                final View v = inflater.inflate(R.layout.custom_dialog_box, null);
+                cBuilder.setView(v)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Snackbar.make(getWindow().getDecorView(),new_message.toString(), Snackbar.LENGTH_LONG);
+                        //snackBarText = snackbar.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+                        //snackbar.setText(new_message.getText().toString());
+                        new_message = (EditText)v.findViewById(R.id.new_message);
+                        snackbarMsg = new_message.getText().toString();
+                    }
+                });
+                cBuilder.show();
                 Log.d("Toolbar", "Option 3 selected");
                 break;
             case R.id.action_four:
