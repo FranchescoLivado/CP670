914ace73ec97ab22ac0de7bfda7827043fd51217
FranchescoLivado <39171544+FranchescoLivado@users.noreply.github.com>
2019-11-03 17:57:51 -0500

    Finished Assignment 3, part 1

--- app/src/main/java/com/example/myapplication/ChatWindow.java
+++ app/src/main/java/com/example/myapplication/ChatWindow.java
@@ -2,8 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,12 +20,16 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static com.example.myapplication.ChatDatabaseHelper.*;
+
+
 public class ChatWindow extends AppCompatActivity {
     Button send;
     EditText search;
     ListView list;
     ArrayList<String> array;
     ChatAdapter messageAdapter;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +41,56 @@ public class ChatWindow extends AppCompatActivity {
         list = (ListView) findViewById(R.id.listview);
         array = new ArrayList<String>();
         messageAdapter = new ChatAdapter(this);
+        //initialize database helper
+        final ChatDatabaseHelper helper = new ChatDatabaseHelper(this);
+
 
         list.setAdapter(messageAdapter);
 
         send.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
+                //add msg from edit text search bar to array
                 array.add(search.getText().toString());
+                //Add msg from edit text search bar to database
+                SQLiteDatabase db = helper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(KEY_MESSAGE, search.getText().toString());
+                db.insert(TABLE_ITEMS,null, values);
+
                 messageAdapter.notifyDataSetChanged();
                 search.setText("");
             }
         });
+        // A3 Edits Begin
+        ChatDatabaseHelper dhHelper = new ChatDatabaseHelper(this);
+        db = dhHelper.getWritableDatabase();
+
+        //use query() or rawQuery() to retrieve any existing chat msgs and add them to the ArrayList "array"
+        String query = "SELECT " + KEY_MESSAGE + " FROM " + TABLE_ITEMS;
+        //call what current activity is and put it to string for Logs
+        String ACTIVITY_NAME = getCallingActivity().toString();
+
+        Cursor c = db.rawQuery(query,null);
+        int colIndex = c.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE); //Do I access KEY_MESSAGE or TABLE_NAME
+
+        c.moveToFirst();
+        while(!c.isAfterLast() ) {
+            String msg = c.getString(colIndex);
+            array.add(msg);
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + c.getString( c.getColumnIndex( ChatDatabaseHelper.KEY_MESSAGE)));
+            Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + c.getColumnCount());
+            c.moveToNext();
+        };
+
+        c.moveToFirst();
+        for (int i = 0; i < c.getCount(); i++){
+            String column = c.getColumnName(colIndex);
+            System.out.print("Column: " + column);
+            c.moveToNext();
+        }
+
+
+
     }
 
     private class ChatAdapter extends ArrayAdapter<String> {
@@ -71,5 +120,10 @@ public class ChatWindow extends AppCompatActivity {
 
             return result;
         }
+    }
+
+    protected void onDestroy(){
+        db.close();
+        super.onDestroy();
     }
 }
